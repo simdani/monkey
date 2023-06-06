99308a9910aa446a5774e6ba7702019d9e062a00
Simonas Daniliauskas <daniliauskas.s@gmail.com>
2023-06-06 22:06:23 +0300

    Remove not need code from `ast`

--- ast/ast.go
+++ ast/ast.go
@@ -20,14 +20,6 @@ type Program struct {
 	Statements []Statement
 }
 
-func (p *Program) TokenLiteral() string {
-	if len(p.Statements) > 0 {
-		return p.Statements[0].TokenLiteral()
-	} else {
-		return ""
-	}
-}
-
 type LetStatement struct {
 	Token token.Token // the token.LET token
 	Name  *Identifier
